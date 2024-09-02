f217ea17008116b611a54cebed08c356b45ee31e
A Meaningful star <ghrlfehd123@naver.com>
2024-09-02 12:08:39 +0900

    edit: toast popup default style

---
+++ src/components/ToastPopup/index.tsx
@@ -0,0 +1,7 @@
+export default function ToastPopup() {
+  return (
+    <div className="fixed bottom-20 w-11/12 bg-[#777B7F] p-5 rounded-lg">
+      <span className="text-white font-semibold">이것은 안내창입니다.</span>
+    </div>
+  )
+}
